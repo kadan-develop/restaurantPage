bd5f7b3413f5913df2edf6d6cf39c2e465b18e2d
Daniel <daniel.kacanski@gmail.com>
2025-09-17 22:12:45 +0200

    fix

--- src/homeContentLoad.js
+++ src/homeContentLoad.js
@@ -8,7 +8,7 @@ const divContent = document.getElementById("content");
 divContent.innerHTML = `
 
         <div class="hero-content">
-            <img class="hero-img" src="./images/zakaria-zayane-0uAVsDcyD0M-unsplash.jpg" alt="" srcset="">
+            <img class="hero-img" src="./src/images/zakaria-zayane-0uAVsDcyD0M-unsplash.jpg" alt="" srcset="">
 
 
             <div class="hero-text">
